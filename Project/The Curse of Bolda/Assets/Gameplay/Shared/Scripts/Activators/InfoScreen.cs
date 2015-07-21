1268c58d812089390ec358df627345412e4280e7
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-07-21 08:22:09 +0100

    Started infoscreen activators

---
+++ Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/Activators/InfoScreen.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Shared.Scripts.Activators
+{
+    public class InfoScreen : MonoBehaviour
+    {
+        private bool _isActive;
+        private bool _canBeToggled;
+        private SpriteRenderer _renderer;
+
+        public Sprite OffSprite;
+        public Sprite OnSprite;
+
+        private void Awake()
+        {
+            _renderer = transform.GetComponent<SpriteRenderer>();
+            _renderer.sprite = OffSprite;
+
+            _canBeToggled = false;
+            _isActive = false;
+        }
+
+        private void Update()
+        {
+            if ((Input.GetKeyDown(KeyCode.S)) && (_canBeToggled))
+            {
+                _isActive = !_isActive;
+                _renderer.sprite = (_isActive ? OnSprite : OffSprite);
+
+                Diagnostics.DiagnosticsDisplay.SetDiagnostic("x", _isActive.ToString());
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            _canBeToggled = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D collider)
+        {
+            _canBeToggled = false;
+        }
+    }
+}
