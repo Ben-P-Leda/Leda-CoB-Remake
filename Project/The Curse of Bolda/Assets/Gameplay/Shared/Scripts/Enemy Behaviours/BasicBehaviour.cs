2987786fbc3a6fc58a7e7e114236c11d7243db66
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-07-22 08:57:07 +0100

    Enemy behaviour base and move between limits behaviour added

---
+++ Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/Enemy Behaviours/BasicBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Shared.Scripts.Enemy_Behaviours
+{
+    public class BasicBehaviour : MonoBehaviour
+    {
+        public int HitPoints;
+        public int ScoreValue;
+        public bool PlayerShotsDoNoDamage;
+        public bool DoesNotStopPlayerShots;
+
+        protected virtual void OnTriggerEnter2D(Collider2D collider)
+        {
+            if ((collider.tag == "Player Shot") && (!DoesNotStopPlayerShots))
+            {
+                if (!PlayerShotsDoNoDamage)
+                {
+                    // TODO:
+                    //      - Get player shot script
+                    //      - Subtract damage value from hit points
+
+                    if (HitPoints < 1)
+                    {
+                        //  - Switch off enemy
+                        //  - Start death effect
+                        //  - Award score
+                    }
+
+                    //      - Switch off player shot
+                }
+            }
+        }
+    }
+}
--- Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/Enemy Behaviours/HorizontalMovementBetweenLimits.cs
+++ Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/Enemy Behaviours/HorizontalMovementBetweenLimits.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class HorizontalMovementBetweenLimits : MonoBehaviour {
+namespace Gameplay.Shared.Scripts.Enemy_Behaviours
+{
+    public class HorizontalMovementBetweenLimits : BasicBehaviour
+    {
+        private Transform _transform;
+        private Rigidbody2D _rigidBody2D;
+        private float _leftSideLimit;
+        private float _rightSideLimit;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        public float LeftSideLimitOffset;
+        public float RightSideLimitOffset;
+        public float Speed;
+
+        private void Awake()
+        {
+            _transform = transform;
+            _rigidBody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void Start()
+        {
+            _leftSideLimit = _transform.position.x - LeftSideLimitOffset;
+            _rightSideLimit = _transform.position.x + RightSideLimitOffset;
+
+            float midPoint = _rightSideLimit - _leftSideLimit;
+            float startDirection = Mathf.Sign(midPoint - _transform.position.x);
+
+            _rigidBody2D.velocity = new Vector2(Speed * startDirection, 0.0f);
+            _transform.localScale = new Vector3(_transform.localScale.x * startDirection, _transform.localScale.y, _transform.localScale.z);
+        }
+
+        private void Update()
+        {
+            if ((Mathf.Sign(_rigidBody2D.velocity.x) < 0) && (_transform.position.x <= _leftSideLimit)) { ChangeDirection(); }
+            if ((Mathf.Sign(_rigidBody2D.velocity.x) > 0) && (_transform.position.x >= _rightSideLimit)) { ChangeDirection(); }
+        }
+
+        private void ChangeDirection()
+        {
+            _rigidBody2D.velocity = new Vector2(-_rigidBody2D.velocity.x, _rigidBody2D.velocity.y);
+            _transform.localScale = new Vector3(-_transform.localScale.x, _transform.localScale.y, _transform.localScale.z);
+        }
+    }
 }
