5397d3fa2e4a340b609435fddcd5cba0a5eb148d
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-07-23 08:54:50 +0100

    Added score HUD

---
+++ Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/Status Display/Score.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+using Shared.Scripts;
+
+namespace Gameplay.Shared.Scripts.Status_Display
+{
+    public class Score : DisplayBase
+    {
+        protected override void Start()
+        {
+            base.Start();
+
+            Rect displayArea = new Rect(
+                (Screen.width - (BackgroundTexture.width * Scaling)) * 0.5f,
+                0.0f,
+                BackgroundTexture.width * Scaling,
+                BackgroundTexture.height * Scaling);
+
+            DisplayArea = displayArea;
+            TextArea = new Rect(displayArea.x, displayArea.y + Text_Vertical_Offset, displayArea.width, displayArea.height - Text_Vertical_Offset);
+            Text = "0";
+        }
+
+        public void Refresh()
+        {
+            Text = CurrentGame.GameData.Score.ToString();
+        }
+
+        private float Text_Vertical_Offset = 10.0f;
+    }
+}
