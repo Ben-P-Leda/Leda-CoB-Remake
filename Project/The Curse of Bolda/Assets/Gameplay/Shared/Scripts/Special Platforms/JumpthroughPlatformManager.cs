d59b3374ae4e9b11521de0625129267ee36dae54
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-07-27 12:09:55 +0100

    Fixed bug in jumpthrough detection

--- Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/Special Platforms/JumpthroughPlatformManager.cs
+++ Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/Special Platforms/JumpthroughPlatformManager.cs
@@ -6,6 +6,7 @@ namespace Gameplay.Shared.Scripts.Special_Platforms
     {
         private JumpthroughPlatformContainer[] _platforms;
         private Transform _playerTransform;
+        private Rigidbody2D _playerRigidBody;
 
         protected JumpthroughPlatformContainer[] Platforms { get { return _platforms; } }
 
@@ -25,24 +26,38 @@ namespace Gameplay.Shared.Scripts.Special_Platforms
             }
 
             _playerTransform = Player.transform;
+            _playerRigidBody = Player.GetComponent<Rigidbody2D>();
         }
 
         private void Update()
         {
-            for (int i = 0; i < _platforms.Length; i++)
+            if (_playerRigidBody.velocity.y <= 0.0f)
             {
-                if (_playerTransform.position.y - Jumpthru_Offset > _platforms[i].Transform.position.y)
+                for (int i = 0; i < _platforms.Length; i++)
                 {
-                    _platforms[i].GameObject.layer = Below_Player_Sorting_Layer;
-                }
-                else
-                {
-                    _platforms[i].GameObject.layer = Above_Player_Sorting_Layer;
+                    if (ShouldApplyPhysicsToPlayer(_platforms[i].Transform.position))
+                    {
+                        _platforms[i].GameObject.layer = Below_Player_Sorting_Layer;
+                    }
+                    else
+                    {
+                        _platforms[i].GameObject.layer = Above_Player_Sorting_Layer;
+                    }
                 }
             }
         }
 
+        private bool ShouldApplyPhysicsToPlayer(Vector3 platformPosition)
+        {
+            if (_playerTransform.position.y - Jumpthru_Offset <= platformPosition.y) { return false; }
+            if (_playerTransform.position.x + Horizontal_Margin < platformPosition.x) { return false; }
+            if (_playerTransform.position.x - Horizontal_Margin > platformPosition.x) { return false; }
+
+            return true;
+        }
+
         private const float Jumpthru_Offset = 0.175f;
+        private const float Horizontal_Margin = 0.35f;
         private const int Above_Player_Sorting_Layer = 9;
         private const int Below_Player_Sorting_Layer = 8;
     }
