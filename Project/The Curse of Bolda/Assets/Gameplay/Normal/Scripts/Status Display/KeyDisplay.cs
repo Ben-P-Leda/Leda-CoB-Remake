3f93b2485257c950220e28f448cf57e44ae02f43
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-07-29 13:14:24 +0100

    Freeze enemy type 1 animation when movement frozen

---
+++ Project/The Curse of Bolda/Assets/Gameplay/Normal/Scripts/Status Display/KeyDisplay.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+using Shared.Scripts;
+using Gameplay.Shared.Scripts.Status_Display;
+
+namespace Gameplay.Shared.Normal.Status_Display
+{
+    public class KeyDisplay : DisplayBase
+    {
+        protected override void Start()
+        {
+            base.Start();
+
+            Rect displayArea = new Rect(
+                Screen.width - ((BackgroundTexture.width + Horizontal_Margin) * Scaling),
+                Screen.height - ((BackgroundTexture.height + Vertical_Margin) * Scaling),
+                BackgroundTexture.width * Scaling,
+                BackgroundTexture.height * Scaling);
+
+            DisplayArea = displayArea;
+            TextArea = new Rect(0, 0, 0, 0);
+        }
+
+        protected override void OnGUI()
+        {
+            if (CurrentGame.GameData.CarryingKey) { base.OnGUI(); }
+        }
+
+        private const float Horizontal_Margin = 200.0f;
+        private const float Vertical_Margin = 19.0f;
+    }
+}
---
+++ Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/Enemy Behaviours/Type01Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Shared.Scripts.Enemy_Behaviours
+{
+    public class Type01Movement : HorizontalMovementBetweenLimits
+    {
+        private Animator _animator;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _animator = GetComponent<Animator>();
+        }
+
+        public override void SetFrozen(bool freeze)
+        {
+            base.SetFrozen(freeze);
+
+            _animator.enabled = !freeze;
+        }
+    }
+}
