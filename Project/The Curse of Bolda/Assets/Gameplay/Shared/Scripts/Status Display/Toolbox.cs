d081be4a41ec1fd8e31a7148652c908d8a23ece7
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-07-31 13:31:19 +0100

    Display refactor, toolbox added

---
+++ Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/Status Display/Toolbox.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+using Shared.Scripts;
+
+namespace Gameplay.Shared.Scripts.Status_Display
+{
+    public class Toolbox : DisplayBase
+    {
+        protected override void Start()
+        {
+            base.Start();
+
+            Rect displayArea = new Rect(
+                Margin * Scaling,
+                Screen.height - ((BackgroundTexture.height + Margin) * Scaling),
+                BackgroundTexture.width * Scaling,
+                BackgroundTexture.height * Scaling);
+
+            DisplayArea = displayArea;
+            TextArea = new Rect(0,0,0,0);
+        }
+
+        private void Update()
+        {
+        }
+
+        protected override void DrawText()
+        {
+        }
+    }
+}
