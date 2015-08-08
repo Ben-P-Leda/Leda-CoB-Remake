56fa59a54b1b6468c0f0586bf954add42126a016
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-08-08 16:56:48 +0100

    Test for paralax BG

--- Project/The Curse of Bolda/Assets/Gameplay/Normal/Scripts/Player Control/TrackingCameraController.cs
+++ Project/The Curse of Bolda/Assets/Gameplay/Normal/Scripts/Player Control/TrackingCameraController.cs
@@ -6,15 +6,20 @@ namespace Gameplay.Normal.Scripts.Player_Control
     {
         private Transform _transform;
         private Transform _objectToTrack;
+        private Transform _bgTransform;
         private Rect _cameraMovementArea;
+        private Vector2 _levelCenter;
+        private Vector2 _bgMovementExtent;
 
         public GameObject ObjectToTrack;
         public GameObject LevelMap;
+        public GameObject ParalaxBackground;
 
         private void Awake()
         {
             _transform = transform;
             _objectToTrack = ObjectToTrack.transform;
+            _bgTransform = ParalaxBackground.transform;
         }
 
         private void Start()
@@ -27,6 +32,15 @@ namespace Gameplay.Normal.Scripts.Player_Control
                 Mathf.Min(LevelMap.transform.position.y + cameraMargins.y, cameraMargins.y),
                 Mathf.Max(mapBoundaries.width - (cameraMargins.x * 2.0f), 0),
                 Mathf.Max(mapBoundaries.height - (cameraMargins.y * 2.0f), 0));
+
+            _levelCenter = new Vector2(
+                LevelMap.transform.position.x + (mapBoundaries.width * 0.5f),
+                LevelMap.transform.position.y + (mapBoundaries.height * 0.5f));
+
+            SpriteRenderer bgSpriteRenderer = ParalaxBackground.GetComponent<SpriteRenderer>();
+            _bgMovementExtent = new Vector2(
+                bgSpriteRenderer.sprite.bounds.extents.x - cameraMargins.x,
+                bgSpriteRenderer.sprite.bounds.extents.y - cameraMargins.y);
         }
 
         private void Update()
@@ -35,6 +49,15 @@ namespace Gameplay.Normal.Scripts.Player_Control
                 Mathf.Clamp(_objectToTrack.position.x, _cameraMovementArea.xMin, _cameraMovementArea.xMax),
                 Mathf.Clamp(_objectToTrack.position.y, _cameraMovementArea.yMin, _cameraMovementArea.yMax),
                 _transform.position.z);
+
+            Vector2 offset = new Vector2(
+                (_transform.position.x - _cameraMovementArea.center.x) / (_cameraMovementArea.width * 0.5f),
+                (_transform.position.y - _cameraMovementArea.center.y) / (_cameraMovementArea.width * 0.5f));
+
+            _bgTransform.position = new Vector3(
+                _transform.position.x - (_bgMovementExtent.x * offset.x),
+                _transform.position.y - (_bgMovementExtent.y * offset.y),  
+                _bgTransform.position.z);
         }
     }
 }
---
+++ Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/BackgroundLayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundLayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Diagnostics.DiagnosticsDisplay.SetDiagnostic("zzz", GetComponent<SpriteRenderer>().sprite.bounds.min.ToString());
+        //Diagnostics.DiagnosticsDisplay.SetDiagnostic("zzz1", GetComponent<SpriteRenderer>().sprite.bounds.max.ToString());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
