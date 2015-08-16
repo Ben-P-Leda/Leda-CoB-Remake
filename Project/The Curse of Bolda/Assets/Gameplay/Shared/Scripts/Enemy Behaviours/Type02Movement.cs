3ad6d0301e89321d0c245cbfaf1370b9b3583732
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-08-16 08:52:48 +0100

    More missile setup

---
+++ Project/The Curse of Bolda/Assets/Gameplay/Shared/Scripts/Enemy Behaviours/Type02Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using Gameplay.Shared.Scripts.Player;
+using Gameplay.Shared.Scripts.Shots;
+
+namespace Gameplay.Shared.Scripts.Enemy_Behaviours
+{
+    public class Type02Movement : Type01Movement
+    {
+        private EnemyMissile _missile;
+
+        public float DetectionRange;
+        public GameObject Missile;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            _missile = Missile.GetComponent<EnemyMissile>();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+    }
+}
