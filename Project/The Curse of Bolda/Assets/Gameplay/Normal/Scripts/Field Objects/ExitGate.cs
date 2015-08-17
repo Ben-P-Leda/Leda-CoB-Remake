c3f619738006c316780551f315ce34ec281218b6
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-08-17 17:58:53 +0100

    Started exit gate

---
+++ Project/The Curse of Bolda/Assets/Gameplay/Normal/Scripts/Field Objects/ExitGate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Normal.Scripts.Field_Objects
+{
+    public class ExitGate : MonoBehaviour
+    {
+        private Sprite _sprite;
+        private SpriteRenderer _spriteRenderer;
+        private Rect _fullSizeRect;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _sprite = _spriteRenderer.sprite;
+            _fullSizeRect = _sprite.textureRect;
+        }
+    }
+}
