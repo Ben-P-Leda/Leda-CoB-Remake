4b0c57c5ba83696203191f59867a75bc5611e62d
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-09-14 18:05:26 +0100

    switched normal levels to normal scripts

--- Project/The Curse of Bolda/Assets/Gameplay/Normal/Scripts/Player/NormalPlayerController.cs
+++ Project/The Curse of Bolda/Assets/Gameplay/Normal/Scripts/Player/NormalPlayerController.cs
@@ -1,8 +1,8 @@
-﻿using Gameplay.Normal.Scripts.Player_Control;
+﻿using Gameplay.Shared.Scripts.Player;
 
 namespace Gameplay.Normal.Scripts.Player
 {
-    public class NormalPlayerController : KevActionController
+    public class NormalPlayerController : InputDrivenPlayer
     {
     }
 }
